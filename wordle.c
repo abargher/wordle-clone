73a234424835542e4b0a5302c484fd126a211982
Alec Bargher <abargher@uchicago.edu>
2023-06-02 13:24:50 -0500

    added file structure for wordle clone

---
+++ wordle.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "wordle.h"
+
+/* Worlde clone function definitions */
+
---
+++ wordle.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+/* Wordle clone header file */
+
+#ifndef _WORDLE_H_
+#define _WORDLE_H_
+
+
+#endif // _WORDLE_H_
